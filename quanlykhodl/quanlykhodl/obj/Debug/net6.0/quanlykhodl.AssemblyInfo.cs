5dbd9a2a5bd451ae40e8c136fb948299c39d7d50
truong086 <91019609+truong086@users.noreply.github.com>
2025-02-15 17:48:53 +0800

    updatepushservernew

--- quanlykhodl/quanlykhodl/obj/Debug/net6.0/quanlykhodl.AssemblyInfo.cs
+++ quanlykhodl/quanlykhodl/obj/Debug/net6.0/quanlykhodl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quanlykhodl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9a0f29ac2b5fcc96266bdba1e9eec427e0be80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("quanlykhodl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quanlykhodl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
